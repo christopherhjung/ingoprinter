dde3a13df9b2e2627999c541b23e1c2a33486d00
christopherhjung <christopher.h.jung@web.de>
2020-03-09 11:31:54 +0100

    remove some thngs

--- klippy/extras/drv8711.py
+++ klippy/extras/drv8711.py
@@ -1,15 +1,8 @@
-# MCP4018 digipot support (via bit-banging)
-#
-# Copyright (C) 2019  Kevin O'Connor <kevin@koconnor.net>
-#
-# This file may be distributed under the terms of the GNU GPLv3 license.
-
 import bus
 
 class drv8711:
     def __init__(self, config):
         self.printer = config.get_printer()
-        self.gcode = self.printer.lookup_object("gcode")
         self.printer.register_event_handler("klippy:connect", self._handle_connect)
         self.spi = bus.MCU_SPI_from_config(config, 3)
 
