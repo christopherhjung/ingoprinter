34839db9492fa1aa5d28fbd6ff2ccce5fa8ec981
christopherhjung <christopher.h.jung@web.de>
2020-03-09 11:28:30 +0100

    refactoring

--- klippy/extras/drv8711.py
+++ klippy/extras/drv8711.py
@@ -26,15 +26,6 @@ class drv8711:
     def send(self, val):
         data = [(val >> 8) & 0xff, val & 0xff]
         self.spi.spi_send(data)
-        response = self.spi.spi_send(data)
-
-    def reverse_mask(self, x):
-        x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1)
-        x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2)
-        x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4)
-        x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8)
-        x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16)
-        return x
 
 def load_config_prefix(config):
     return drv8711(config)
